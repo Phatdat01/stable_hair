65097be8276e1168ecf710273e8a3996cb5a502b
Phatdat01 <phatdatlht2001@gmail.com>
2025-06-04 09:56:17 +0700

    feat: chnage model api

--- api.py
+++ api.py
@@ -6,13 +6,130 @@ import logging
 import numpy as np
 from PIL import Image
 
-from hair_swap import HairFast, get_parser
+import torch
+import cv2
+from omegaconf import OmegaConf
 
-model_args = get_parser()
-hair_fast = HairFast(model_args.parse_args([]))
+from diffusers import DDIMScheduler, UniPCMultistepScheduler
+from diffusers.models import UNet2DConditionModel
+from ref_encoder.latent_controlnet import ControlNetModel
+from ref_encoder.adapter import *
+from ref_encoder.reference_unet import ref_unet
+from utils.pipeline import StableHairPipeline
+from utils.pipeline_cn import StableDiffusionControlNetPipeline
+
+# from hair_swap import HairFast, get_parser
+
+# model_args = get_parser()
+# hair_fast = HairFast(model_args.parse_args([]))
 # model_parser = get_parser()
 # model_args, _ = model_parser.parse_known_args()
 # hair_fast = HairFast(model_args)
+
+class StableHair:
+    def __init__(self, config="./configs/hair_transfer.yaml", device="cuda", weight_dtype=torch.float32) -> None:
+        print("Initializing Stable Hair Pipeline...")
+        self.config = OmegaConf.load(config)
+        self.device = device
+
+        ### Load vae controlnet
+        unet = UNet2DConditionModel.from_pretrained(self.config.pretrained_model_path, subfolder="unet").to(device)
+        controlnet = ControlNetModel.from_unet(unet).to(device)
+        _state_dict = torch.load(os.path.join(self.config.pretrained_folder, self.config.controlnet_path))
+        controlnet.load_state_dict(_state_dict, strict=False)
+        controlnet.to(weight_dtype)
+
+        ### >>> create pipeline >>> ###
+        self.pipeline = StableHairPipeline.from_pretrained(
+            self.config.pretrained_model_path,
+            controlnet=controlnet,
+            safety_checker=None,
+            torch_dtype=weight_dtype,
+        ).to(device)
+        self.pipeline.scheduler = DDIMScheduler.from_config(self.pipeline.scheduler.config)
+
+        ### load Hair encoder/adapter
+        self.hair_encoder = ref_unet.from_pretrained(self.config.pretrained_model_path, subfolder="unet").to(device)
+        _state_dict = torch.load(os.path.join(self.config.pretrained_folder, self.config.encoder_path))
+        self.hair_encoder.load_state_dict(_state_dict, strict=False)
+        self.hair_adapter = adapter_injection(self.pipeline.unet, device=self.device, dtype=torch.float16, use_resampler=False)
+        _state_dict = torch.load(os.path.join(self.config.pretrained_folder, self.config.adapter_path))
+        self.hair_adapter.load_state_dict(_state_dict, strict=False)
+
+        ### load bald converter
+        bald_converter = ControlNetModel.from_unet(unet).to(device)
+        _state_dict = torch.load(self.config.bald_converter_path)
+        bald_converter.load_state_dict(_state_dict, strict=False)
+        bald_converter.to(dtype=weight_dtype)
+        del unet
+
+        ### create pipeline for hair removal
+        self.remove_hair_pipeline = StableDiffusionControlNetPipeline.from_pretrained(
+            self.config.pretrained_model_path,
+            controlnet=bald_converter,
+            safety_checker=None,
+            torch_dtype=weight_dtype,
+        )
+        self.remove_hair_pipeline.scheduler = UniPCMultistepScheduler.from_config(self.remove_hair_pipeline.scheduler.config)
+        self.remove_hair_pipeline = self.remove_hair_pipeline.to(device)
+
+        ### move to fp16
+        self.hair_encoder.to(weight_dtype)
+        self.hair_adapter.to(weight_dtype)
+
+        print("Initialization Done!")
+
+    def Hair_Transfer(self, source_image, reference_image, random_seed, step, guidance_scale, scale, controlnet_conditioning_scale):
+        prompt = ""
+        n_prompt = ""
+        random_seed = int(random_seed)
+        step = int(step)
+        guidance_scale = float(guidance_scale)
+        scale = float(scale)
+        controlnet_conditioning_scale = float(controlnet_conditioning_scale)
+
+        # load imgs
+        H, W, C = source_image.shape
+
+        # generate images
+        set_scale(self.pipeline.unet, scale)
+        generator = torch.Generator(device="cuda")
+        generator.manual_seed(random_seed)
+        sample = self.pipeline(
+            prompt,
+            negative_prompt=n_prompt,
+            num_inference_steps=step,
+            guidance_scale=guidance_scale,
+            width=W,
+            height=H,
+            controlnet_condition=source_image,
+            controlnet_conditioning_scale=controlnet_conditioning_scale,
+            generator=generator,
+            reference_encoder=self.hair_encoder,
+            ref_image=reference_image,
+        ).samples
+        return sample, source_image, reference_image
+
+    def get_bald(self, id_image, scale):
+        H, W = id_image.size
+        scale = float(scale)
+        image = self.remove_hair_pipeline(
+            prompt="",
+            negative_prompt="",
+            num_inference_steps=30,
+            guidance_scale=1.5,
+            width=W,
+            height=H,
+            image=id_image,
+            controlnet_conditioning_scale=scale,
+            generator=None,
+        ).images[0]
+
+        return image
+
+
+model = StableHair(config="./configs/hair_transfer.yaml", weight_dtype=torch.float32)
+
 
 def resize_image(image, target_size=(1024, 1024)):
     """Resize the image to the target size (e.g., 1024x1024)."""
@@ -121,15 +238,36 @@ def get_wig():
         target_size = (1024, 1024)
         face_image = resize_image(ensure_rgb(face_image), target_size)
         shape_image = resize_image(ensure_rgb(shape_image), target_size)
-        color_image = shape_image  # Use shape as color too
+
+        face_image = ensure_rgb(face_image).resize((512, 512))
+        ref_hair = ensure_rgb(shape_image).resize((512, 512))
 
         # Call your hair swap function
-        final_tensor = hair_fast.swap(face_image, shape_image, color_image, align=align)
+        id_image_np = np.array(face_image).astype('uint8')
+        ref_hair_np = np.array(ref_hair).astype('uint8')
 
-        # Convert tensor to PIL
-        final_image = final_tensor.squeeze(0).permute(1, 2, 0).cpu().detach().numpy()
-        final_image = (final_image * 255).astype(np.uint8)
-        final_pil_image = Image.fromarray(final_image)
+        # Convert back to PIL to apply model pipeline
+        id_image = Image.fromarray(id_image_np, 'RGB')
+        ref_hair = Image.fromarray(ref_hair_np, 'RGB')
+
+        # Step 1: Get bald image
+        id_image_bald = model.get_bald(id_image, 1)
+
+        # Step 2: Convert inputs to numpy arrays
+        id_image_bald = np.array(id_image_bald)
+        ref_hair = np.array(ref_hair)
+
+        # Step 3: Hair Transfer
+        image, source_image, reference_image = model.Hair_Transfer(
+            source_image=id_image_bald,
+            reference_image=ref_hair,
+            random_seed=-1,
+            step=30,
+            guidance_scale=1.5,
+            scale=1.0,
+            controlnet_conditioning_scale=1
+        )
+        final_pil_image = Image.fromarray((image * 255).astype(np.uint8))
 
         # Prepare response
         img_byte_arr = io.BytesIO()
@@ -184,4 +322,4 @@ def test_image():
     return Response(img_byte_arr, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
